7777de03fc296327eedc9ff830f72db9c6a7cba4
Jane Kim <jane@giveforward.com>
2017-01-30 01:22:17 -0600

    create bill and associated archive

--- app/services/bills_service.rb
+++ app/services/bills_service.rb
@@ -1,5 +1,29 @@
 module BillsService
 
+  def self.find_or_create(record)
+    bill = Bill.find_by(api_id: record["bill_id"]) || Bill.new
+    bill.vote_id        = record["vote_id"]
+    bill.short_title    = record["short_title"]
+    bill.bill_type      = record["bill_type"]
+    bill.number         = record["number"]
+    bill.chamber        = record["chamber"]
+    bill.introduced_on  = record["introduced_on"]
+    bill.last_action_at = record["last_action_at"]
+    bill.save
+
+    find_or_create_associated_result(bill, record) if record.has_key?("history")
+  end
+
+  def self.find_or_create_associated_result(bill, record)
+    attrs = record["history"]
+    if archive = bill.archive
+      archive.update(attrs)
+    else
+      archive = Archive.create(attrs)
+      bill.archive = archive
+    end
+  end
+
   def self.format_date(datestring)
     return nil unless datestring
     date = DateTime.parse(datestring)
